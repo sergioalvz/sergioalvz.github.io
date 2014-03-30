d1e394962fafad61dd5a33a2656479a86c9b9b4b
Sergio Álvarez <sergioalvarezsuarez@gmail.com>
2014-03-30 17:36:48 +0200

    Added new utilitie

---
+++ utilities/tweetheat/javascript/map.js
@@ -0,0 +1,41 @@
+var MapModule = (function(){
+  var heatpoints = new google.maps.MVCArray([]);
+
+  var addPointToHeatmap = function(lat, lng) {
+    var point = new google.maps.LatLng(lat, lng);
+    heatpoints.push(point);
+  };
+
+  var processTsvFile = function(event){
+    var tsv = event.target.result;
+    tsv.split("\n").forEach(function(row){
+      var columns = row.split('\t');
+      var lat = parseFloat(columns[1]);
+      var lng = parseFloat(columns[2]);
+      addPointToHeatmap(lat, lng);
+    });
+  };
+
+  var install = function(){
+    var mapOptions = { zoom: 2, center: new google.maps.LatLng(43.354810, -5.851805)};
+    var map = new google.maps.Map($('.map')[0], mapOptions);
+    var heatmap = new google.maps.visualization.HeatmapLayer({
+      data: heatpoints
+    });
+    heatmap.setMap(map);
+  };
+
+  var loadFiles = function(files) {
+    var reader = new FileReader();
+    for(var i = 0; i < files.length; i++) {
+      var currentFile = files[i];
+      reader.onload = processTsvFile;
+      reader.readAsText(currentFile);
+    }
+  };
+
+  return {
+    install: install,
+    loadFiles: loadFiles
+  };
+})();
