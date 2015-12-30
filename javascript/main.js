81d92fd92795af9890132528e9eb7c919c53f61e
Sergio Álvarez <sergioalvarezsuarez@gmail.com>
2015-12-30 14:05:26 +0100

    Add rolling words

--- javascript/main.js
+++ javascript/main.js
@@ -1,8 +1,26 @@
-$('[go-to]').click(function(event) {
+$( '[go-to]' ).click(function(event) {
   event.preventDefault();
 
   var target = $(this).attr('go-to');
   var $target = $(target);
 
-  $('html, body').animate({ scrollTop: $target.offset().top }, 200);
+  $( 'html, body' ).animate({ scrollTop: $target.offset().top }, 200);
 });
+
+function changeAdjective() {
+  var adjectives = [ 'fast', 'effective', 'consistent', 'responsive', 'friendly', 'maintainable' ];
+  var index = 0;
+
+  return function() {
+    if(index > adjectives.length - 1) {
+      index = 0;
+    }
+
+    var adjective = adjectives[index];
+    $( '.js-rolling-word' ).text(adjective);
+
+    index++;
+  };
+}
+
+setInterval(changeAdjective(), 5000);
